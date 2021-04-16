8ef36f9224fbd7e1e341c3f1855000254cf9448f
japsasir <58173215+japsasir@users.noreply.github.com>
2021-04-16 13:07:37 +0200

    Arreglado correo.

--- wp-cli.sh
+++ wp-cli.sh
@@ -84,7 +84,7 @@ chown -R www-data:www-data /var/www/html
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
 
 # Instalamos Wordpress con la configuración. Recordatorio de actualizar la IP en la lista de variables.
-wp core install --url=$IP_PUBLICA --title="IAW Jose Padilla" --admin_user=admin --admin_password=admin_password --admin_email=@gmail.com --allow-root
+wp core install --url=$IP_PUBLICA --title="IAW Jose Padilla" --admin_user=admin --admin_password=admin_password --admin_email=jose@gmail.com --allow-root
 
 # Nos dirigimos a la instalación de wp para poder ejecutar la herramienta cli sin problemas
 cd /var/www/html
